d3729d7e90bd1311c06c8acf163dbe0e9be3f734
Mfknudsen <35879356+Mfknudsen@users.noreply.github.com>
2019-12-02 12:55:44 +0100

    +SkødeDat

---
+++ SkødeData.js
@@ -0,0 +1,579 @@
+class SkødeData{
+    constructor(){
+        this.DataObject = {};
+    }
+
+    fåData(i){
+        if(i == 1){
+            this.DataObject = {
+                Price: 1200,
+
+                FarveR: 0,
+                FarveG: 0,
+                FarveB: 255,
+                
+                Navn: "Lokale 11", 
+                                
+                Leje: 0,
+                Hus1: 0,
+                Hus2: 0,
+                Hus3: 0,
+                Hus4: 0,
+                Hotel: 0,
+
+                HusPris: 0,
+                HotelPris: 0,
+
+                PantVærdi: 0
+            }
+
+            return this.DataObject;
+        }
+
+        if(i == 2){
+            this.DataObject = {
+                Price: 1200,
+
+                FarveR: 0,
+                FarveG: 0,
+                FarveB: 255,
+
+                Navn: "Lokale 12",
+                
+                Leje: 0,
+                Hus1: 0,
+                Hus2: 0,
+                Hus3: 0,
+                Hus4: 0,
+                Hotel: 0,
+
+                HusPris: 0,
+                HotelPris: 0,
+
+                PantVærdi: 0
+            }
+            
+            return this.DataObject;
+        }
+
+        if(i == 3){
+            this.DataObject = {
+                Price: 2000,
+
+                FarveR: 255,
+                FarveG: 255 * 0.4,
+                FarveB: 255 * 0.25,
+
+                Navn: "Lokale 13",
+                
+                Leje: 0,
+                Hus1: 0,
+                Hus2: 0,
+                Hus3: 0,
+                Hus4: 0,
+                Hotel: 0,
+
+                HusPris: 0,
+                HotelPris: 0,
+
+                PantVærdi: 0
+            }
+
+            return this.DataObject;
+        }
+
+        if(i == 4){
+            this.DataObject = {
+                Price: 2000,
+
+                FarveR: 255,
+                FarveG: 255 * 0.4,
+                FarveB: 255 * 0.25,
+
+                Navn: "Lokale 14",
+                
+                Leje: 0,
+                Hus1: 0,
+                Hus2: 0,
+                Hus3: 0,
+                Hus4: 0,
+                Hotel: 0,
+
+                HusPris: 0,
+                HotelPris: 0,
+
+                PantVærdi: 0
+            }
+
+            return this.DataObject;
+        }
+
+        if(i == 5){
+            this.DataObject = {
+                Price: 2000,
+
+                FarveR: 255,
+                FarveG: 255 * 0.4,
+                FarveB: 255 * 0.25,
+
+                Navn: "Lokale 15",
+                
+                Leje: 0,
+                Hus1: 0,
+                Hus2: 0,
+                Hus3: 0,
+                Hus4: 0,
+                Hotel: 0,
+
+                HusPris: 0,
+                HotelPris: 0,
+
+                PantVærdi: 0
+            }
+
+            return this.DataObject;
+        }
+
+        if(i == 6){
+            this.DataObject = {
+                Price: 2800,
+
+                FarveR: 255,
+                FarveG: 255,
+                FarveB: 255 * 0.1,
+
+                Navn: "Lokale 16",
+
+                Leje: 0,
+                Hus1: 0,
+                Hus2: 0,
+                Hus3: 0,
+                Hus4: 0,
+                Hotel: 0,
+
+                HusPris: 0,
+                HotelPris: 0,
+
+                PantVærdi: 0
+            }
+
+            return this.DataObject;
+        }
+
+        if(i == 7){
+            this.DataObject = {
+                Price: 2800,
+
+                FarveR: 255,
+                FarveG: 255,
+                FarveB: 255 * 0.1,
+
+                Navn: "Lokale 17",
+
+                Leje: 0,
+                Hus1: 0,
+                Hus2: 0,
+                Hus3: 0,
+                Hus4: 0,
+                Hotel: 0,
+
+                HusPris: 0,
+                HotelPris: 0,
+
+                PantVærdi: 0
+            }
+
+            return this.DataObject;
+        }
+
+        if(i == 8){
+            this.DataObject = {
+                Price: 3200,
+
+                FarveR: 255,
+                FarveG: 255,
+                FarveB: 255 * 0.1,
+
+                Navn: "Lokale 18",
+
+                Leje: 0,
+                Hus1: 0,
+                Hus2: 0,
+                Hus3: 0,
+                Hus4: 0,
+                Hotel: 0,
+
+                HusPris: 0,
+                HotelPris: 0,
+
+                PantVærdi: 0
+            }
+
+            return this.DataObject;
+        }
+
+        if(i == 9){
+            this.DataObject = {
+                Price: 3600,
+
+                FarveR: 255 * 0.6,
+                FarveG: 255 * 0.55,
+                FarveB: 255 * 0.525,
+
+                Navn: "Lokale 21",
+
+                Leje: 0,
+                Hus1: 0,
+                Hus2: 0,
+                Hus3: 0,
+                Hus4: 0,
+                Hotel: 0,
+
+                HusPris: 0,
+                HotelPris: 0,
+
+                PantVærdi: 0
+            }
+
+            return this.DataObject;
+        }
+
+        if(i == 10){
+            this.DataObject = {
+                Price: 3600,
+
+                FarveR: 255 * 0.6,
+                FarveG: 255 * 0.55,
+                FarveB: 255 * 0.525,
+
+                Navn: "Lokale 22",
+
+                Leje: 0,
+                Hus1: 0,
+                Hus2: 0,
+                Hus3: 0,
+                Hus4: 0,
+                Hotel: 0,
+
+                HusPris: 0,
+                HotelPris: 0,
+
+                PantVærdi: 0
+            }
+
+            return this.DataObject;
+        }
+
+        if(i == 11){
+            this.DataObject = {
+                Price: 4000,
+
+                FarveR: 255 * 0.6,
+                FarveG: 255 * 0.55,
+                FarveB: 255 * 0.525,
+
+                Navn: "Lokale 23",
+
+                Leje: 0,
+                Hus1: 0,
+                Hus2: 0,
+                Hus3: 0,
+                Hus4: 0,
+                Hotel: 0,
+
+                HusPris: 0,
+                HotelPris: 0,
+
+                PantVærdi: 0
+            }
+
+            return this.DataObject;
+        }
+
+        if(i == 12){
+            this.DataObject = {
+                Price: 4400,
+
+                FarveR: 255,
+                FarveG: 0,
+                FarveB: 0,
+
+                Navn: "Lokale 30",
+
+                Leje: 0,
+                Hus1: 0,
+                Hus2: 0,
+                Hus3: 0,
+                Hus4: 0,
+                Hotel: 0,
+
+                HusPris: 0,
+                HotelPris: 0,
+
+                PantVærdi: 0
+            }
+
+            return this.DataObject;
+        }
+
+        if(i == 13){
+            this.DataObject = {
+                Price: 4400,
+
+                FarveR: 255,
+                FarveG: 0,
+                FarveB: 0,
+
+                Navn: "Lokale 31",
+
+                Leje: 0,
+                Hus1: 0,
+                Hus2: 0,
+                Hus3: 0,
+                Hus4: 0,
+                Hotel: 0,
+
+                HusPris: 0,
+                HotelPris: 0,
+
+                PantVærdi: 0
+            }
+
+            return this.DataObject;
+        }
+
+        if(i == 14){
+            this.DataObject = {
+                Price: 4800,
+
+                FarveR: 255,
+                FarveG: 0,
+                FarveB: 0,
+
+                Navn: "Lokale 32",
+
+                Leje: 0,
+                Hus1: 0,
+                Hus2: 0,
+                Hus3: 0,
+                Hus4: 0,
+                Hotel: 0,
+
+                HusPris: 0,
+                HotelPris: 0,
+
+                PantVærdi: 0
+            }
+
+            return this.DataObject;
+        }
+
+        if(i == 15){
+            this.DataObject = {
+                Price: 5200,
+                
+                FarveR: 255,
+                FarveG: 255,
+                FarveB: 255,
+
+                Navn: "Lokale 71",
+
+                Leje: 450,
+                Hus1: 2200,
+                Hus2: 6600,
+                Hus3: 16000,
+                Hus4: 19500,
+                Hotel: 23000,
+
+                HusPris: 3000,
+                HotelPris: 3000,
+
+                PantVærdi: 2600
+            }
+
+            return this.DataObject;
+        }
+
+        if(i == 16){
+            this.DataObject = {
+                Price: 5200,
+
+                FarveR: 255,
+                FarveG: 255,
+                FarveB: 255,
+
+                Navn: "Lokale 72",
+
+                Leje: 0,
+                Hus1: 0,
+                Hus2: 0,
+                Hus3: 0,
+                Hus4: 0,
+                Hotel: 0,
+
+                HusPris: 0,
+                HotelPris: 0,
+
+                PantVærdi: 0
+            }
+
+            return this.DataObject;
+        }
+
+        if(i == 17){
+            this.DataObject = {
+                Price: 5500,
+
+                FarveR: 255,
+                FarveG: 255,
+                FarveB: 255,
+
+                Navn: "Lokale 73",
+
+                Leje: 0,
+                Hus1: 0,
+                Hus2: 0,
+                Hus3: 0,
+                Hus4: 0,
+                Hotel: 0,
+
+                HusPris: 0,
+                HotelPris: 0,
+
+                PantVærdi: 0
+            }
+
+            return this.DataObject;
+        }
+
+        if(i == 18){
+            this.DataObject = {
+                Price: 6000,
+
+                FarveR: 255,
+                FarveG: 255,
+                FarveB: 255 * 0.1,
+
+                Navn: "Lokale 60",
+
+                Leje: 0,
+                Hus1: 0,
+                Hus2: 0,
+                Hus3: 0,
+                Hus4: 0,
+                Hotel: 0,
+
+                HusPris: 0,
+                HotelPris: 0,
+
+                PantVærdi: 0
+            }
+
+            return this.DataObject;
+        }
+
+        if(i == 19){
+            this.DataObject = {
+                Price: 6000,
+
+                FarveR: 255,
+                FarveG: 255,
+                FarveB: 255 * 0.1,
+
+                Navn: "Lokale 61",
+
+                Leje: 0,
+                Hus1: 0,
+                Hus2: 0,
+                Hus3: 0,
+                Hus4: 0,
+                Hotel: 0,
+
+                HusPris: 0,
+                HotelPris: 0,
+
+                PantVærdi: 0
+            }
+
+            return this.DataObject;
+        }
+
+        if(i == 20){
+            this.DataObject = {
+                Price: 6400,
+
+                FarveR: 255,
+                FarveG: 255,
+                FarveB: 255 * 0.1,
+
+                Navn: "Lokale 62",
+                
+                Leje: 0,
+                Hus1: 0,
+                Hus2: 0,
+                Hus3: 0,
+                Hus4: 0,
+                Hotel: 0,
+
+                HusPris: 0,
+                HotelPris: 0,
+
+                PantVærdi: 0
+            }
+
+            return this.DataObject;
+        }
+
+        if(i == 21){
+            this.DataObject = {
+                Price: 7000,
+
+                FarveR: 255 * 0.385,
+                FarveG: 255 * 0.28,
+                FarveB: 255 * 0.35,
+
+                Navn: "Nordsalen",
+
+                Leje: 0,
+                Hus1: 0,
+                Hus2: 0,
+                Hus3: 0,
+                Hus4: 0,
+                Hotel: 0,
+
+                HusPris: 0,
+                HotelPris: 0,
+
+                PantVærdi: 0
+            }
+
+            return this.DataObject;
+        }
+
+        if(i == 22){
+            this.DataObject = {
+                Price: 8000,
+
+                FarveR: 255 * 0.385,
+                FarveG: 255 * 0.28,
+                FarveB: 255 * 0.35,
+
+                Navn: "Kantinen",
+
+                Leje: 0,
+                Hus1: 0,
+                Hus2: 0,
+                Hus3: 0,
+                Hus4: 0,
+                Hotel: 0,
+
+                HusPris: 0,
+                HotelPris: 0,
+
+                PantVærdi: 0
+            }
+
+            return this.DataObject;
+        }
+    }
+}
